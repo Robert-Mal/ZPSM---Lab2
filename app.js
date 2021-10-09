697cb7965553804fe247489fbbd827fb6b997852
Robert-Mal <33180@student.pwsztar.edu.pl>
2021-10-09 17:03:23 +0200

    function findMin and findMax added

--- app.js
+++ app.js
@@ -5,3 +5,14 @@ function calculateAverage(arrayOfNumbers) {
 }
 
 console.log(calculateAverage([1,2,3,34]));
+
+function findMin(arrayOfNumbers) {
+    return _.min(arrayOfNumbers);
+}
+
+function findMax(arrayOfNumbers) {
+    return _.max(arrayOfNumbers);
+}
+
+console.log(findMin([1,2,3,4]));
+console.log(findMax([1,2,3,4]));
